bec98a9fc90d2a695dc8396e33d0b01ad8aa50d5
Sofia Emteryd <sofia.emteryd@gmail.com>
2023-01-10 15:11:38 +0100

    trying set-up

---
+++ final-project/src/App.js
@@ -0,0 +1,11 @@
+import './Styles/main.css';
+import Home from "./Pages/Home"
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
---
+++ final-project/src/Components/Main.jsx
@@ -0,0 +1,8 @@
+
+const Main = () => {
+    <>
+        <h1>HEJHEJ</h1>
+    </>
+}
+
+export default Main;
